7a74a34b950b0b16c99c114bc8d8ddd8acdcfe35
nariogulu <98317524+nariogulu@users.noreply.github.com>
2022-05-16 16:23:32 +0100

    command button added

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -5,6 +5,13 @@
 
 /* global global, Office, self, window */
 
+// The initialize function must be run each time a new page is loaded
+(function () {
+  Office.initialize = function (reason) {
+    //If you need to initialize something you can do so here.
+  };
+})();
+
 Office.onReady(() => {
   // If needed, Office.js is ready to be called
 });
@@ -28,6 +35,47 @@ function action(event) {
   event.completed();
 }
 
+function update(event) {
+  //Consult Office.js API reference to see all you can do. This just shows the simplest action.
+
+  Word.run(async (context) => {
+    /**
+     * Insert your Word code here
+     *
+     */
+    const url = "https://jsonplaceholder.typicode.com/todos/1";
+    const response = await fetch(url);
+
+    //Expect that status code is in 200-299 range
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    console.log("Response: " + data.id);
+
+    let doc = context.document;
+    let paragraphs = context.document.body.paragraphs;
+    paragraphs.load("$none");
+
+    await context.sync();
+
+    let contentControls = context.document.contentControls.getByTag("amount");
+    contentControls.load("text");
+
+    await context.sync();
+
+    //copied from docs, will refactor
+    let a = Math.random();
+    for (let i = 0; i < contentControls.items.length; i++) {
+      contentControls.items[i].insertText((data.id * a * 1000).toString(), "Replace");
+    }
+
+    await context.sync();
+  });
+  //Required, call event.completed to let the platform know you are done processing.
+  event.completed();
+}
+
 function getGlobal() {
   return typeof self !== "undefined"
     ? self
@@ -41,4 +89,5 @@ function getGlobal() {
 const g = getGlobal();
 
 // The add-in command functions need to be available in global scope
-g.action = action;
+g.action = update;
+g.update = update;
